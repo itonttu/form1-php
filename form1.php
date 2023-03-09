a9a637dbce565639d7c574509c2046e8af2d6aa0
itonttu <42998227+itonttu@users.noreply.github.com>
2023-03-09 10:55:23 +0900

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo '<pre>';
+        // print_r($_POST);
+        // echo '</pre>';
+    
+        $f1 = $_POST['food1'];
+        $f2 = $_POST['food2'];
+        $f1 = htmlspecialchars( $f1, ENT_QUOTES, 'UTF-8' );
+        $f2 = htmlspecialchars( $f2, ENT_QUOTES, 'UTF-8' );
+        echo 'あなたは「', $f1,'」が好きなんですね。<br>', PHP_EOL;
+        echo '「', $f2,'」も好きなんですね。<br>', PHP_EOL;
+    ?>
+</body>
+</html>
